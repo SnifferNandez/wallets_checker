9539f874dd2e38834eb8f98f0b02b52034a340f1
SnifferNandez <C4Fdez+31337@gmail.com>
2017-11-18 12:07:30 -0500

    Funcionality and wallets

--- checker.py
+++ checker.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python
+import urllib2
+import json
+import time
+
+currency = {'BTC':'https://insight.bitpay.com/api/',
+            'BCH':'https://cashexplorer.bitcoin.com/api/'}
+
+# Create in wallets.txt one wallet per line
+# Commit using wallets from https://docs.google.com/spreadsheets/d/1xTROekDerP1TPOB3SOD_1bbQr580BPqbhF3YHdO96pw
+file = open('wallets.txt', 'r') 
+for wallet in file: 
+  for k,v in currency.items():
+    if True:
+    #try:
+      walletInfo = json.loads(urllib2.urlopen(v+'addr/'+wallet).read())
+      if walletInfo['balance'] != 0:
+      	info = walletInfo['addrStr'] + ' {:f}' + ' ' + k 
+        print info.format(walletInfo['balance']) # Use format to avoid scientific notation
+    #except:
+     #pass
+  time.sleep(0.05) # Limit to 20 request per second
